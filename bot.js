277b5179c5954f584f94fbe7a34c963797afb7b5
ksomkyacc <36326959+Baron010@users.noreply.github.com>
2018-09-16 03:24:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -677,7 +677,10 @@ client.on("message", message => {
 
     }
       });
-
+client.on('message', message=>{
+if(message.content.startsWith(prefix+"userinfo")) {
+if(!message.channel.guild) return;
+if(message.author.bot) return;
 let uinfo = message.mentions.users.first() || message.author
 if(uinfo.bot) {
 var type = "BOT"
