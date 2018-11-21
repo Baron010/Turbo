95a6da236bc8c1b798503af4901b9e25fea0a10b
ksomkyacc <36326959+Baron010@users.noreply.github.com>
2018-11-21 21:31:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5010,4 +5010,15 @@ client.on('message',async message => {
     }
   }
 });
+client.on('message', message => {
+if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith(prefix+'users')){
+ if(!message.author.id === '437892991047434241') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 client.login(process.env.BOT_TOKEN);
